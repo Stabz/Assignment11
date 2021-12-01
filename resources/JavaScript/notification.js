8c25da509754b88f62120824ad45a3ce6d1c99bb
stabz <dmitrokaraush@gmail.com>
2021-12-01 23:27:11 +0100

    deleted:    resources/JS 	new file:   resources/JavaScript/notification.js 	new file:   resources/JavaScript/validation.js

---
+++ resources/JavaScript/notification.js
@@ -0,0 +1,20 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+const { Variables } = require("camunda-external-task-client-js");
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Process Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'Notification'
+client.subscribe("Notification", async function({ task, taskService }) {
+   
+   console.log("The task failed successfully");
+   
+  // complete the task
+  await taskService.complete(task);
+  
+});
